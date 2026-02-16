ea793ccc1fe1b8b658f7717c51429266e336803e
nahmij <nahmijavaheri@gmail.com>
2026-02-15 16:55:30 -0800

    Updated implementation for Transaction

--- src/main/model/Transaction.java
+++ src/main/model/Transaction.java
@@ -1,18 +1,19 @@
 package model;
+
 import java.time.LocalDateTime;
 
 public class Transaction {
-    private final double amount; //amount of transaction
-    private final LocalDateTime dateTime; //time and date of transaction
-    private final transactionType type; //type of transaction (INCOME, EXPENSE, or REFUND)
-    private final transactionCategory category; //category of transaction (FOOD, SHOPPING, etc.)
+    private final double amount; // amount of transaction
+    private final LocalDateTime dateTime; // time and date of transaction
+    private final transactionType type; // type of transaction (INCOME, EXPENSE, or REFUND)
+    private final transactionCategory category; // category of transaction (FOOD, SHOPPING, etc.)
 
-    /* 
-    * REQUIRES: amount > 0.0
-    * MODIFIES: this
-    * EFFECTS: creates transaction with specified positive amount, time and date, 
-    *          transaction type, and description.
-    */
+    /*
+     * REQUIRES: amount > 0.0
+     * MODIFIES: this
+     * EFFECTS: creates transaction with specified positive amount, time and date,
+     * transaction type, and description.
+     */
     public Transaction(double amount, LocalDateTime dateTime, transactionType type, transactionCategory category) {
         this.amount = amount;
         this.dateTime = dateTime;
@@ -20,18 +21,22 @@ public class Transaction {
         this.category = category;
     }
 
-    /* 
-    * REQUIRES: amount > 0.0
-    * MODIFIES: this
-    * EFFECTS: creates transaction with specified positive amount, current time and date,
-    *          specified transaction type, and unspecified description.
-    */
+    /*
+     * REQUIRES: amount > 0.0
+     * MODIFIES: this
+     * EFFECTS: creates transaction with specified positive amount, current time and
+     * date,
+     * specified transaction type, and unspecified description.
+     */
     public Transaction(double amount, LocalDateTime dateTime, transactionType type) {
         this.amount = amount;
         this.dateTime = dateTime;
         this.type = type;
-            if (type == transactionType.INCOME || type == transactionType.REFUND) this.category = transactionCategory.OTHER_IN;
-        else this.category = transactionCategory.OTHER_OUT;
+        if (type == transactionType.INCOME || type == transactionType.REFUND) {
+            this.category = transactionCategory.OTHER_IN;
+        } else {
+            this.category = transactionCategory.OTHER_OUT;
+        }
     }
 
     public double getAmount() {
@@ -45,11 +50,13 @@ public class Transaction {
     public transactionCategory getCategory() {
         return category;
     }
- 
+
     public transactionType getTransactionType() {
         return type;
     }
 
+    // EFFECTS: Returns signed amount of transaction, negative if it is an expense,
+    // positive otherwise
     public double getSignedAmount() {
         if (this.type == transactionType.EXPENSE) {
             return -this.amount;
